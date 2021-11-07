9ea75ea872417a2394930650dc6f37550a9d7ab7
Nathan Tsai <nathan@albernidesign.com>
2021-11-06 17:51:36 -0700

    Hide the button explictly

--- src/Plugin/WebformHandler/WebformPaypalSmartButtons.php
+++ src/Plugin/WebformHandler/WebformPaypalSmartButtons.php
@@ -66,7 +66,7 @@ class WebformPaypalSmartButtons extends WebformHandlerBase {
     }
     
     if (isset($form['elements']['actions'])) {
-      $form['elements']['actions']['#attributes']['class'][] = 'visually-hidden'; // We want users to click the paypal button
+      $form['elements']['actions']['#attributes']['class'][] = 'hidden'; // We want users to click the paypal button
       // $form['elements']['actions']['#submit__attributes']['class'][] = 'js--webformPaypalCheckoutSubmitButton'; // @TODO make constants in webform api
 
       unset($form['elements']['actions']['submit']);
